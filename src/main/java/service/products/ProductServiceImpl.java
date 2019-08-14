75f5ac3cfcc10daae3387a86bdaba299d4c3e2c3
Kev <kdowdy1021@gmail.com>
2019-08-14 08:47:40 -0400

    Updated business cases for Products. Created tests for service layer.

--- src/main/java/service/products/ProductServiceImpl.java
+++ src/main/java/service/products/ProductServiceImpl.java
@@ -4,6 +4,7 @@
 package service.products;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import dao.ProductDAO;
 import dao.entity.Product;
@@ -26,11 +27,44 @@ public final class ProductServiceImpl implements ProductService {
 	}
 
 	@Override
-	public BigDecimal scanAndGetPrice(String id) {
+	public List<Product> getProducts() {
+		return this.productDao.getProducts();
+		
+	}
+
+	@Override
+	public Product scanAndGetProduct(String id) {
 		Product p = this.productDao.getProductByID(id);
 		if(p != null) {
-			return p.getPrice();
+			return p;
 		}
 		return null;
+	}
+
+	@Override
+	public int createProduct(BigDecimal price, String name, String supplierID, boolean sale, String productID) {
+		
+		int count = this.productDao.addProduct(productID, price, name, supplierID, sale);
+		if(count == 1) {
+			return count;
+		}
+		return 0;
+	}
+
+	@Override
+	public int updateProduct(BigDecimal price, String name, String supplierID, boolean sale, String productID) {
+		int count = this.productDao.updateProduct(price, name, supplierID, sale, productID);
+		if(count == 1) {
+			return count;
+		}
+		return 0;	}
+
+	@Override
+	public int deleteProduct(String pid) {
+		int count = this.productDao.deleteProduct(pid);
+		if(count == 1) {
+			return count;
+		}
+		return 0;
 	}
 }
